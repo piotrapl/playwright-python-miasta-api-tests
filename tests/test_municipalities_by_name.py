58d04c2d81fc6433a4071f7f68e2e9f96b27a948
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-01-27 18:47:58 +0100

    komentarze dot. testów negatywnych

--- tests/test_municipalities_by_name.py
+++ tests/test_municipalities_by_name.py
@@ -3,6 +3,8 @@ from tests.test_data import POSITIVE_CITIES, NEGATIVE_CITIES
 
 # Testy dla endpointu pobierającego jednostki samorządowe po nazwie miasta
 # Asercje sprawdzają, czy odpowiedzi API są zgodne z oczekiwaniami dla istniejących i nieistniejących nazw miast.
+# @pytest.mark.parametrize("city", POSITIVE_CITIES)
+# - to odpowiednik pętli for dla testów jednostkowych - uruchamia test dla każdej wartości w POSITIVE_CITIES
 
 @pytest.mark.parametrize("city", POSITIVE_CITIES)
 def test_get_municipality_by_name_should_return_data(api_request, city):
@@ -15,6 +17,8 @@ def test_get_municipality_by_name_should_return_data(api_request, city):
     assert "data" in body
     assert len(body["data"]) > 0
 
+# Test negatywny dla tego samego endpointu
+# Spawdza, czy odpowiedź API jest poprawna dla NIEISTNIEJĄCEJ nazwy miasta
 
 @pytest.mark.parametrize("city", NEGATIVE_CITIES)
 def test_get_municipality_by_non_existing_name_should_return_404(api_request, city):
@@ -30,8 +34,9 @@ def test_get_municipality_by_non_existing_name_should_return_404(api_request, ci
     assert body["error"].get("code") in {"NOT_FOUND", "BAD_REQUEST"}
     assert "message" in body["error"]
 
-
-def test_get_municipality_with_empty_name_should_return_404_and_bad_request(api_request):
+# Test negatywny dla tego samego endpointu
+# Sprawdza, czy odpowiedź API jest poprawna dla PUSTEJ nazwy miasta
+    def test_get_municipality_with_empty_name_should_return_404_and_bad_request(api_request):
     response = api_request.get("/api/v1/municipalities/name/")
 
     # As requested
